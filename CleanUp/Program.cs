86426889e5274b0f487b359071f6702aafe54bce
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2021-09-02 12:14:17 +0200

    Add project files.

---
+++ CleanUp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+
+namespace CleanUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data.GetDiskMetadata();
+            Data.GetHardDiskSerialNumber();
+            Logic.CPU();
+            Logic.hovedLager();
+            Logic.test();
+            Logic.testhest();
+            Console.WriteLine("process søgning");
+            Logic.LISTAllServices();
+            Console.ReadKey();
+
+        } //slut main
+    }
+}
